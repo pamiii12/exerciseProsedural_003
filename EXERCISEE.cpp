d4924b8cde5e26ff3d5c0a30b411291ab1be9e8f
pamiii12 <fahmiadityaa03@gmail.com>
2023-03-29 15:06:20 +0700

    exercise1

--- EXERCISEE.cpp
+++ EXERCISEE.cpp
@@ -1,20 +1,39 @@
-// EXERCISEE.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int jumlahMahasiswa = 20;
+    string namaMahasiswa[jumlahMahasiswa];
+    int nilaiBahasainggris[jumlahMahasiswa];
+    int nilaiMatematika[jumlahMahasiswa];
+    string status[jumlahMahasiswa];
+
+
+    for (int i = 0; i < jumlahMahasiswa; i++) {
+        cout << "Masukkan nama mahasiswa: ";
+        cin >> namaMahasiswa[i];
+        cout << "Masukkan nilai bahasa Inggris: ";
+        cin >> nilaiBahasainggris[i];
+        cout << "Masukkan nilai Matematika: ";
+        cin >> nilaiMatematika[i];
+        cout << endl;
+
+        int rata_rata = (nilaiBahasainggris[i] + nilaiMatematika[i]) / 2;
+
+        if (rata_rata >= 70) {
+            status[i] = "Diterima";
+        }
+        else {
+            status[i] = "Ditolak";
+        }
+    }
+
+
+    cout << "Nama\t\tStatus" << endl;
+    for (int i = 0; i < jumlahMahasiswa; i++) {
+        cout << namaMahasiswa[i] << "\t\t" << status[i] << endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
